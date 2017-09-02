9fdc10e99e92f08824bc7825c894bd21495a963f
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-09-02 12:31:01 +0530

    Create Question_11.cpp

---
+++ Question_11.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a;
+cout << "Enter week number : ";
+cin >> a;
+switch(a)
+      {
+      	case 1 : cout << "SUNDAY";
+      	         break;
+      	case 2 : cout << "MONDAY";
+      	         break;
+      	case 3 : cout << "TUESDAY";
+      	         break;
+      	case 4 : cout << "WEDNESDAY";
+      	         break;
+      	case 5 : cout << "THURSDAY";
+      	         break;
+      	case 6 : cout << "FRIDAY";
+      	         break;
+      	case 7 : cout << "SATURDAY";
+      	         break;
+      	default : cout << "INVALID WEEK NUMBER";         
+      }
+return 0;
+}
