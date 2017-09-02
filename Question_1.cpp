01f4b69437b1241d1a4f5e9a7014c5539cab6cbf
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-09-02 11:37:17 +0530

    Create Question_1.cpp

---
+++ Question_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+float a,b;
+cout << "Enter first & second number respectively : ";
+cin >> a >> b;
+if(a>b)
+  cout << "Greater number = " << a;
+  else
+  cout << "Greater number = " << b;
+	return 0;
+}
