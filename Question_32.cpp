6ac27a934c859806d206c750c5a5c46a63256a15
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-09-03 18:24:32 +0530

    Update Question_32.cpp

--- Question_32.cpp
+++ Question_32.cpp
@@ -6,11 +6,10 @@ int a,i,j;
 cout << "Enter number : ";
 cin >> a;
 j = a;
-while(j!=0)
+while(j>10)
 {
-     j = j / 10;
-     ++i;
+    j = j / 10;
 }
-cout << "First digit = " << a/(10*(i-1)) << "\nLast digit = " << a%(10*(i-1));
+cout << "First digit = " << j << "\nLast digit = " << a%10;
 return 0;
 }
