db638904c03c090c0987dc6af18081d355541041
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-09-03 18:26:22 +0530

    Update Question_33.cpp

--- Question_33.cpp
+++ Question_33.cpp
@@ -6,11 +6,10 @@ int a,i,j;
 cout << "Enter number : ";
 cin >> a;
 j = a;
-while(j!=0)
+while(j>10)
 {
      j = j / 10;
-     ++i;
 }
-cout << "Sum of first & last digit = " << a/(10*(i-1)) + a%(10*(i-1));
+cout << "Sum of first & last digit = " << j + (a%10);
 return 0;
 }
