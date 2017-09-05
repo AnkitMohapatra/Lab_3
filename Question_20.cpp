de94e9b508d5f2d82d4ade5a840375ecc4606778
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-09-05 16:42:35 +0530

    Update Question_20.cpp

--- Question_20.cpp
+++ Question_20.cpp
@@ -8,7 +8,7 @@ cin >> bs;
 if(bs<=10000)
   gs = bs + (0.2*bs) + (0.8*bs);
   else
-   if(bs<=20000)
+   if(bs<=20000 & bs<=10000)
      gs = bs + (0.25*bs) + (0.9*bs);
      else
       gs = bs + (0.3*bs) + (0.95*bs);
