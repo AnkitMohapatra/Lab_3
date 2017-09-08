ca67c00177bf4f47115d1a474f38c76682a55c86
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-09-08 18:46:28 +0530

    Create Question_61.cpp

---
+++ Question_61.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+long a,num,rem1,rem2,dec=0,oct=0,b1=1,b2=1;
+cout << "Enter Binary number : ";
+cin >> a;
+num = a;
+while(num>0)
+{
+     rem1 = num % 10;
+     dec = dec + rem1*b1;
+     b1 = b1 * 2;
+     num = num / 10;
+}
+cout << dec;
+return 0;
+}
