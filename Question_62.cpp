286ac89e61cb45a54be062af122cc1bdd9b9cdfc
Ankit Mohapatra <31150694+AnkitMohapatra@users.noreply.github.com>
2017-09-14 23:20:57 +0530

    Update Question_62.cpp

--- Question_62.cpp
+++ Question_62.cpp
@@ -1,43 +1,36 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-int a,num,rem,remm,hex=0,dec=0,b=1,n=1;
-cout << "Enter Binary number : ";
-cin >> a;
-num = a;
-while(num>0)
+int main()
 {
-     rem = num % 10;
-     dec = dec + rem*b;
-     b = b * 2;
-     num = num / 10;
-}
-while(dec>0)
-{
-    remm = dec % 16;
-    hex = hex + remm*n;
-    n = n * 10;
-    dec = dec / 16;
-}
-if(hex>=10 & hex<=15)
-{
-switch(hex)
-{
-    case 10: cout << "A";
-             break;
-    case 11: cout << "B";
-             break;
-    case 12: cout << "C";
-             break;
-    case 13: cout << "D";
-             break;
-    case 14: cout << "E";
-             break;
-    case 15: cout << "F";
-}
-}
-else
-    cout << hex;
-return 0;
+ long bin, dec = 0, rem, num, base = 1;
+ cout << "\nEnter the binary number(1s and 0s) : ";
+ cin >> num;
+ bin = num;
+ while(num > 0)
+      {
+        rem = num % 10;
+        dec = dec + rem * base;
+        base = base * 2;
+        num = num / 10;
+      }
+ long int quo;
+ int i=1,j,temp;
+ char hexa[100];
+ quo = dec;
+ while(quo!=0)
+      {
+       temp = quo % 16;
+       if(temp < 10)
+          temp =temp + 48;
+       else
+          temp = temp + 55;
+ hexa[i++]= temp;
+ quo = quo / 16;
+      }
+ cout << "\nEquivalent hexadecimal number of " << bin << " is : ";
+ for(j = i -1 ; j> 0 ; j--)
+ cout << hexa[j];
+ cout << endl;
+ return 0;
 }
